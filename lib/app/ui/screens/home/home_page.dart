54c9f5c8b07b1599e089360573a4401902453104
tk <tykeaboyloy@gmail.com>
2024-07-10 00:18:11 +0700

    close #18 added playlist grid on homepage

--- lib/app/ui/screens/home/home_page.dart
+++ lib/app/ui/screens/home/home_page.dart
@@ -3,13 +3,16 @@ import 'package:get/get.dart';
 import 'package:mobile_project/app/controllers/sp_controller/home_controller.dart';
 import 'package:mobile_project/app/data/models/sp_model/user_model.dart';
 import 'package:mobile_project/app/ui/screens/music/music_page.dart';
+import 'package:mobile_project/app/ui/screens/home/widgets/playlist_card.dart';
 import 'package:mobile_project/app/ui/screens/podcast/podcast_page.dart';
+import 'package:mobile_project/app/data/models/sp_model/playlist_model.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<PlaylistModel> playlists = controller.getPlaylists();
     final UserModel user = controller.getUser();
     final HomeController homeController =
         Get.put(HomeController(), permanent: false);
@@ -19,10 +22,17 @@ class HomePage extends GetView<HomeController> {
         appBar: AppBar(
           title: Row(
             children: [
-              Text(user.name),
+              CircleAvatar(
+                backgroundImage: user.photoUrl.isNotEmpty
+                    ? NetworkImage(user.photoUrl)
+                    : null,
+                child: user.photoUrl.isEmpty
+                    ? Text(user.name[0].toUpperCase())
+                    : null,
+              ),
               ButtonBar(
                 children: [
-                  const SizedBox(width: 8),
+                  const SizedBox(width: 4),
                   buttonBarItem(homeController, 'All', 0),
                   buttonBarItem(homeController, 'Music', 1),
                   buttonBarItem(homeController, 'Podcasts', 2),
@@ -34,10 +44,12 @@ class HomePage extends GetView<HomeController> {
         body: Obx(
           () => IndexedStack(
             index: homeController.tabIndex.value,
-            children: const [
-              Placeholder(),
-              MusicPage(),
-              PodcastPage(),
+            children: [
+              HomePageBody(
+                playlists: playlists,
+              ),
+              const MusicPage(),
+              const PodcastPage(),
             ],
           ),
         ),
@@ -66,6 +78,45 @@ class HomePage extends GetView<HomeController> {
                 : Colors.white,
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HomePageBody extends StatelessWidget {
+  final List playlists;
+  const HomePageBody({super.key, required this.playlists});
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          GridView.builder(
+            padding: const EdgeInsets.all(10),
+            shrinkWrap:
+                true, // Needed to use GridView inside SingleChildScrollView
+            physics:
+                const NeverScrollableScrollPhysics(), // To prevent grid view from scrolling
+            itemCount: playlists.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Number of columns
+              crossAxisSpacing: 2, // Horizontal space between cards
+              mainAxisSpacing: 2, // Vertical space between cards
+              childAspectRatio:
+                  2.9, // Adjust this value to change the card aspect ratio
+            ),
+            itemBuilder: (context, index) {
+              var playlist = playlists[index];
+              return PlaylistCard(
+                imageUrl: playlist.imageUrl,
+                name: playlist.name,
+              );
+            },
+          ),
+        ],
       ),
     );
   }
