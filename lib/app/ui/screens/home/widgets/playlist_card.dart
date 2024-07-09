227d279188dcad43bb2b08a19af6bafc0200b48d
tk <tykeaboyloy@gmail.com>
2024-07-10 00:18:11 +0700

    close #18 added playlist grid on homepage

---
+++ lib/app/ui/screens/home/widgets/playlist_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class PlaylistCard extends StatelessWidget {
+  final String name;
+  final String imageUrl;
+
+  const PlaylistCard({
+    super.key,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: IntrinsicHeight(
+        // Ensures the row's height matches the tallest widget
+        child: Row(
+          children: [
+            AspectRatio(
+              // Makes the image square
+              aspectRatio: 1 / 1, // Aspect ratio of 1:1 for a square
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              // Allows text to fill the remaining space
+              child: Text(
+                name,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
