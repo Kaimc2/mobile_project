cbccd93812bc7b5a8eb1d5ebeabf2dc736d8739b
Prak Pichey <picheyprak@gmail.com>
2024-07-11 10:49:35 +0700

    add sticky app bar to music detail screen

--- lib/app/ui/screens/music/music_detail.dart
+++ lib/app/ui/screens/music/music_detail.dart
@@ -4,139 +4,184 @@ import 'package:mobile_project/app/controllers/sp_controller/music_page_controll
 import 'package:mobile_project/app/data/models/sp_model/music_model.dart';
 import 'package:mobile_project/app/ui/screens/music/widgets/row.dart';
 
-class MusicDetail extends StatelessWidget {
+class MusicDetail extends StatefulWidget {
   const MusicDetail({super.key});
+
+  @override
+  State<MusicDetail> createState() => _MusicDetailState();
+}
+
+class _MusicDetailState extends State<MusicDetail> {
+  final ScrollController _scrollController = ScrollController();
+  bool _isAppBarCollapsed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      setState(() {
+        _isAppBarCollapsed = _scrollController.hasClients &&
+            _scrollController.offset > (310 - kToolbarHeight);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final List<MusicModel> music = MusicPageController().getMusic();
 
     return Scaffold(
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Container(
-          padding: const EdgeInsets.only(
-            top: 48,
-            left: 16,
-            right: 16,
-            bottom: 8,
-          ),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color(0x00b3b3b3).withOpacity(0.8),
-                const Color(0x00535353).withOpacity(0.3),
-                const Color(0x00121212).withOpacity(0.8),
-                const Color(0x00121212).withOpacity(0.8),
-                const Color(0x00121212).withOpacity(0.8),
-                const Color(0x00121212).withOpacity(0.8),
-                const Color(0x00121212).withOpacity(0.8),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Stack(
-                children: [
-                  Positioned(
-                    top: 0,
-                    left: -10,
-                    child: IconButton(
-                      splashColor: Colors.transparent,
-                      highlightColor: Colors.transparent,
-                      hoverColor: Colors.transparent,
-                      onPressed: () {
-                        Get.offAllNamed('/', arguments: {'tabIndex': 1});
-                      },
-                      icon: const Icon(
-                        Icons.chevron_left,
-                        color: Colors.white,
-                        size: 35,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16.0),
-                    child: Center(
-                      child: Image.network(
-                        'https://hololive.hololivepro.com/wp-content/uploads/2021/08/ina_violet_jk-e1661929568573.png',
-                        width: 270,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'VIOLET',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Row(
-                children: [
-                  CircleAvatar(
-                    radius: 12,
-                    backgroundImage: NetworkImage(
-                      'https://linkstorage.linkfire.com/medialinks/images/9a69880e-286f-4b9e-b454-7d0734acfc03/artwork-440x440.jpg',
-                    ),
-                  ),
-                  SizedBox(width: 8),
-                  Text('Ninomae Ina\'nis'),
-                ],
-              ),
-              const SizedBox(height: 8),
-              Text(
-                'Single · 2021',
-                style: TextStyle(
-                  color: Colors.grey[400],
-                ),
-              ),
-              const SizedBox(height: 12),
-              buildActionBar(),
-              buildInfo(
-                title: 'Featuring',
-                content:
-                    'VIOLET - Twilight ver. · りゅーーっときてきゅーーっ!!! · Kanalumi · and more',
-              ),
-              buildInfo(
-                title: 'We added',
-                content:
-                    'マリン出航!! · Sparks of Joy · Daydream · A New Start · 毒杯スワロウ',
-              ),
-              const SizedBox(height: 16),
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('6 August 2021'),
-                  SizedBox(height: 16),
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 24,
-                        backgroundImage: NetworkImage(
-                          'https://linkstorage.linkfire.com/medialinks/images/9a69880e-286f-4b9e-b454-7d0734acfc03/artwork-440x440.jpg',
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Text('Ninomae Ina\'nis'),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                ],
-              ),
-              buildConcertCard(),
-              const SizedBox(height: 40),
-              MusicRow(heading: 'More by Ninomae Ina\'nis', music: music),
-              const SizedBox(height: 20),
-              MusicRow(heading: 'You might also like', music: music),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color(0x00b3b3b3).withOpacity(0.6),
+              const Color(0x00535353).withOpacity(0.7),
+              const Color(0x00121212).withOpacity(0.8),
+              const Color(0x00121212).withOpacity(0.8),
             ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
+        ),
+        child: CustomScrollView(
+          controller: _scrollController,
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverAppBar(
+              backgroundColor: _isAppBarCollapsed
+                  ? const Color(0x00121212).withOpacity(1)
+                  : Colors.transparent,
+              pinned: true,
+              leading: IconButton(
+                hoverColor: Colors.transparent,
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onPressed: () {
+                  Get.offAllNamed('/', arguments: {'tabIndex': 1});
+                },
+                icon: const Icon(
+                  Icons.chevron_left_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              expandedHeight: 310,
+              flexibleSpace: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  final double height = constraints.biggest.height;
+                  final double percentage = (height - kToolbarHeight) / 310;
+
+                  return FlexibleSpaceBar(
+                    titlePadding: EdgeInsets.only(
+                      top: percentage > 0.5 ? 110.0 : 20.0,
+                      left: 20.0,
+                      right: 20.0,
+                      bottom: percentage > 0.5 ? 20.0 : 20.0,
+                    ),
+                    centerTitle: true,
+                    title: percentage > 0.5
+                        ? Center(
+                            child: Image.network(
+                              'https://hololive.hololivepro.com/wp-content/uploads/2021/08/ina_violet_jk-e1661929568573.png',
+                              width: 270,
+                            ),
+                          )
+                        : const Text(
+                            'VIOLET',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                  );
+                },
+              ),
+            ),
+
+            // Actual details
+            SliverToBoxAdapter(
+              child: Container(
+                padding: const EdgeInsets.only(
+                  top: 0,
+                  left: 16,
+                  right: 16,
+                  bottom: 8,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 16),
+                    const Text(
+                      'VIOLET',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 12,
+                          backgroundImage: NetworkImage(
+                            'https://linkstorage.linkfire.com/medialinks/images/9a69880e-286f-4b9e-b454-7d0734acfc03/artwork-440x440.jpg',
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        Text('Ninomae Ina\'nis'),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Single · 2021',
+                      style: TextStyle(
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    buildActionBar(),
+                    buildInfo(
+                      title: 'Featuring',
+                      content:
+                          'VIOLET - Twilight ver. · りゅーーっときてきゅーーっ!!! · Kanalumi · and more',
+                    ),
+                    buildInfo(
+                      title: 'We added',
+                      content:
+                          'マリン出航!! · Sparks of Joy · Daydream · A New Start · 毒杯スワロウ',
+                    ),
+                    const SizedBox(height: 16),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('6 August 2021'),
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 24,
+                              backgroundImage: NetworkImage(
+                                'https://linkstorage.linkfire.com/medialinks/images/9a69880e-286f-4b9e-b454-7d0734acfc03/artwork-440x440.jpg',
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Text('Ninomae Ina\'nis'),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                      ],
+                    ),
+                    buildConcertCard(),
+                    const SizedBox(height: 40),
+                    MusicRow(heading: 'More by Ninomae Ina\'nis', music: music),
+                    const SizedBox(height: 20),
+                    MusicRow(heading: 'You might also like', music: music),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
