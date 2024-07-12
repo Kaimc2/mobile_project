393696710088843ad94353d827ba925312fc89b9
tk <tykeaboyloy@gmail.com>
2024-07-12 16:58:05 +0700

    close #22 added popular artists widget

---
+++ lib/app/ui/screens/home/widgets/artist.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Artist extends StatelessWidget {
+  final String name;
+  final String imageUrl;
+
+  const Artist({
+    super.key,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ClipOval(
+            child: Image.network(
+              imageUrl,
+              width: 150, // Specify the width for the image
+              height: 150, // Specify the height for the image
+              fit: BoxFit.cover, // Cover the entire widget area
+            ),
+          ),
+          const SizedBox(
+              height: 10), // Add some space between the image and the name
+          Text(name,
+              style:
+                  const TextStyle(fontSize: 18)), // Display the artist's name
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/ui/screens/home/widgets/popular_artists.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_project/app/ui/screens/home/widgets/artist.dart';
+
+class PopularArtists extends StatelessWidget {
+  const PopularArtists({
+    super.key,
+    required this.artists,
+  });
+
+  final List artists;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Popular artists",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+          const SizedBox(
+            height: 4,
+          ),
+          SizedBox(
+            height: 250,
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              scrollDirection:
+                  Axis.horizontal, // Set scroll direction to horizontal
+              itemCount: artists.length, // Use artists list
+              itemBuilder: (context, index) {
+                var artist = artists[index]; // Use artist data
+                return Container(
+                  margin: const EdgeInsets.only(
+                      right: 20), // Add space between items
+                  child: Artist(
+                    imageUrl: artist.imageUrl,
+                    name: artist.name,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
